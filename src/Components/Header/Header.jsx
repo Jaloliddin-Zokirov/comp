a2d72ec7d717b0e6446add093bf9b39e202ca53d
Jaloliddin Zokirov <jaloliddinzokirov@Jaloliddins-MacBook-Pro.local>
2024-08-15 23:43:46 +0500

    nmadir

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,6 +3,10 @@ import styles from "./Header.module.scss";
 import logo from "../../assets/Logo-Header.png";
 import icon from "../../assets/Icon.png";
 import shrap from "../../assets/Shape.png";
+import arrowDown from "../../assets/Down.png";
+import compare from "../../assets/compare.png";
+import like from "../../assets/like.png";
+import basket from "../../assets/basket.png";
 import { Link } from "react-router-dom";
 import Menu from "./Menu/Menu";
 import LogReg from "../LogReg/LogReg";
@@ -68,8 +72,68 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <div className={styles.down}></div>
-        {/* <img src={logo} width={164} height={42} alt="site logo" /> */}
+        <div className={styles.down}>
+          <div className="container">
+            <ul className={styles.down__list}>
+              <li className={styles.down__item}>
+                <img src={logo} width={164} height={42} alt="site logo" />
+              </li>
+              <li className={styles.down__item}>katalog</li>
+              <li className={styles.down__item}>
+                <div className={styles.search}>search</div>
+              </li>
+              <li className={styles.down__item}>
+                <a className={styles.tel__number} href="tel:+0500658796">
+                  050 065 87 96
+                </a>
+                <img
+                  className={styles.tel__numberImg}
+                  src={arrowDown}
+                  width={32}
+                  height={32}
+                  alt="arrow down"
+                />
+              </li>
+              <li className={styles.line}></li>
+              <li className={styles.down__item}>
+                <div className={styles.iconBox}>
+                  <img
+                    src={compare}
+                    width={40}
+                    height={40}
+                    alt="to compare icon" 
+                    className={styles.compare__img}
+                  />
+                  <span className={styles.afterCycle}>23</span>
+                </div>
+              </li>
+              <li className={styles.down__item}>
+                <div className={styles.iconBox}>
+                  <img
+                    src={like}
+                    width={40}
+                    height={40}
+                    alt="like icon"
+                    className={styles.like__img}
+                  />
+                  <span className={styles.afterCycle}>23</span>
+                </div>
+              </li>
+              <li className={styles.down__item}>
+                <div className={styles.iconBox}>
+                  <img
+                    src={basket}
+                    width={40}
+                    height={40}
+                    alt="basket icon"
+                    className={styles.basket__img}
+                  />
+                  <span className={styles.afterCycle}>23</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </header>
       <Menu check={checkMenu} setCheck={setcheckMenu} />
       <LogReg />
